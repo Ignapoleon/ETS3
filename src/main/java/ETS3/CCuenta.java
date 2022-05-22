621739a09d121c6a4363132d40669071cc8324a2
Ignacio Tolosa <Ignacio Tolosa@192.168.1.209>
2022-05-22 18:20:55 +0100

    Ignacio Tolosa práctica de tarea ETS4 CARGA INICIAl

--- src/main/java/ETS3/CCuenta.java
+++ src/main/java/ETS3/CCuenta.java
@@ -2,7 +2,9 @@ package ETS3;
 
 public class CCuenta {
 
-
+/**
+ * IGNACIO TOLOSA GARCIA COMENTARIO AÑADIDO PARA ACTUALIZAR
+ */
     private String nombre;
     private String cuenta;
     private double saldo;
